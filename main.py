6fa6f36d70f28d9134f33426c766dd1257048c7f
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-22 06:59:51 +0530

    Create encrypt function to shift characters in text

---
+++ main.py
@@ -0,0 +1,13 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(original_text, shift_number):
+    encrypted_text = ""
+    for char in original_text:
+        shifted_char_index = alphabet.index(char) + shift_number
+        shifted_char = alphabet[shifted_char_index]
+        encrypted_text += shifted_char
+    print(encrypted_text)
