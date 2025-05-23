ee0283d0d53445745e440960392594d1710be8dc
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-23 19:27:23 +0530

    Add simple menu for user interaction

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ print(logo)
 
 def caesar(original_text, shift_amount, encode_or_decode):
     output_text = ""
-    if encode_or_decode == "decode":
+    if encode_or_decode == "2":
         shift_amount *= -1
 
     for letter in original_text:
@@ -16,7 +16,10 @@ def caesar(original_text, shift_amount, encode_or_decode):
             output_text += alphabet[shifted_position]
         else:
             output_text += letter
-    print(f"Here is the {encode_or_decode}d result: {output_text}")
+    if encode_or_decode == "1":
+        print(f"Here is the encoded result: {output_text}")
+    elif encode_or_decode == "2":
+        print(f"Here is the decoded result: {output_text}")
 
 def brute_force_decode(original_text):
     for shift_amount in range(1, 26):
@@ -31,14 +34,14 @@ def brute_force_decode(original_text):
         print(f"shift amount: {shift_amount}\ndecoded text: {decoded_text}\n")
 
 while True:
-    direction = input("\nType 'encode' to encrypt, type 'decode' to decrypt or 'brute' to decode any message\n").lower()
+    direction = input("\nWhat would you like to do?\n1. Encode a message\n2. Decode a message\n3. Brute force a message\n4. Exit\nEnter your choice: ").lower()
     text = input("Type your message:\n").lower()
-    if direction == "brute":
+    if direction == "3":
         brute_force_decode(text)
     else:
         shift = int(input("Type the shift number:\n"))
         caesar(text, shift, direction)
-    go_again = input("Do you want to use caesar cipher again? (y/n): ")
+    go_again = input("\nDo you want to use caesar cipher again? (y/n): ")
     if go_again == "n":
         print("Goodbye...I guess")
         break
